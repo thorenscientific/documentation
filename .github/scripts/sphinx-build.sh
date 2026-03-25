thorenscientific/documentation

commit 02ae91ddac6bf0fa771301d90135a72e509101ee

.github: collect sphinx warns as annotation
Add sphinx-build.sh that collects warnings to a file, then formats as
GitHub Annotations.

Signed-off-by: Jorge Marques <jorge.marques@analog.com>

.github/scripts/sphinx-build.sh
@@ -1,0 +1,47 @@
+#!/bin/bash
+
+_file() {
+	local f="$1"
+	f="${f#$PWD/}"
+	echo "docs/$f"
+}
+
+sphinx_build() {
+	local err=0
+	local step_name="sphinx"
+	local warnfile=$(mktemp -t sphinx.XXX)
+	local docs=${1:-docs}
+
+	pushd $docs
+	sphinx-build -b html $j_ -w  "$warnfile" . _build/html -W --keep-going || err=$?
+        rm -rf _build/html/_sources
+	popd
+
+	while IFS= read -r line; do
+		if [[ "$line" =~ ^(.+):([0-9]+):\ (WARNING|CRITICAL|ERROR):\ (.*)$ ]]; then
+			file="${BASH_REMATCH[1]}"
+			lineno="${BASH_REMATCH[2]}"
+			level="${BASH_REMATCH[3]}"
+			msg="${BASH_REMATCH[4]}"
+			[[ "$level" == "WARNING" ]] && type="warning" || type="error"
+			echo "::$type file=$(_file "$file"),line=$lineno::$step_name: $msg"
+		elif [[ "$line" =~ ^(.+)::\ (WARNING|CRITICAL|ERROR):\ (.*)$ ]]; then
+			file="${BASH_REMATCH[1]}"
+			level="${BASH_REMATCH[2]}"
+			msg="${BASH_REMATCH[3]}"
+			[[ "$level" == "WARNING" ]] && type="warning" || type="error"
+			echo "::$type file=$(_file "$file"),line=0::$step_name: $msg"
+		elif [[ "$line" =~ ^(.+):\ (WARNING|CRITICAL|ERROR):\ (.*)$ ]]; then
+			file="${BASH_REMATCH[1]}"
+			level="${BASH_REMATCH[2]}"
+			msg="${BASH_REMATCH[3]}"
+			[[ "$level" == "WARNING" ]] && type="warning" || type="error"
+			echo "::$type file=$(_file "$file"),line=0::$step_name: $msg"
+		fi
+	done < "$warnfile"
+	rm $warnfile
+
+	return $err
+}
+
+

.github/workflows/top-level.yml
@@ -24,8 +24,9 @@ jobs:
         pip install -r requirements.txt --upgrade
 
     - name: Build doc
-      working-directory: docs
       run: |
+        source .github/scripts/sphinx-build.sh
+
         export GIT_LFS_TO_LINKS="true"
         export GIT_ORG_REPOSITORY="${{ github.repository }}"
         [ "${{ github.event_name }}" = "pull_request" ] \
@@ -34,8 +35,7 @@ jobs:
 
         [[ "${{ runner.debug }}" != "1" ]] && j_="-jauto"
 
-        make html SPHINXOPTS="$j_ -W --keep-going"
-        rm -rf _build/html/_sources
+        sphinx_build
 
     - name: Store the generated doc
       uses: actions/upload-artifact@v4

commit 1b66c8b348549b0edd41df6589663219ed6bb6b0

.github: sphinx-build: Fix wrong relpath
Fixes: 02ae91ddac6b (".github: collect sphinx warns as annotation")
Signed-off-by: Jorge Marques <jorge.marques@analog.com>

.github/scripts/sphinx-build.sh
@@ -3,7 +3,7 @@
 _file() {
 	local f="$1"
 	f="${f#$PWD/}"
-	echo "docs/$f"
+	echo "$f"
 }
 
 sphinx_build() {